8d3505f80562611a93c855a50071eb64afaa98ff
zagPatryk <zag.patryk@gmail.com>
2020-12-29 13:21:42 +0100

    Commit

---
+++ src/main/java/com/drmed/drmed/DrmedApplication.java
@@ -0,0 +1,13 @@
+package com.drmed.drmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrmedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrmedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drmed/drmed/DrmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drmed.drmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrmedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
